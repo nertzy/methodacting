d927a794d8ca8608bbf96c7ac4300ae5ea696de1
nertzy <nertzy@0ec1f860-a52e-0410-ac07-8d77aaf29911>
2007-04-21 22:11:26 +0000

    Add scaffolding for Actor object

---
+++ test/functional/actors_controller_test.rb
@@ -0,0 +1,57 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'actors_controller'
+
+# Re-raise errors caught by the controller.
+class ActorsController; def rescue_action(e) raise e end; end
+
+class ActorsControllerTest < Test::Unit::TestCase
+  fixtures :actors
+
+  def setup
+    @controller = ActorsController.new
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+  def test_should_get_index
+    get :index
+    assert_response :success
+    assert assigns(:actors)
+  end
+
+  def test_should_get_new
+    get :new
+    assert_response :success
+  end
+  
+  def test_should_create_actor
+    old_count = Actor.count
+    post :create, :actor => { }
+    assert_equal old_count+1, Actor.count
+    
+    assert_redirected_to actor_path(assigns(:actor))
+  end
+
+  def test_should_show_actor
+    get :show, :id => 1
+    assert_response :success
+  end
+
+  def test_should_get_edit
+    get :edit, :id => 1
+    assert_response :success
+  end
+  
+  def test_should_update_actor
+    put :update, :id => 1, :actor => { }
+    assert_redirected_to actor_path(assigns(:actor))
+  end
+  
+  def test_should_destroy_actor
+    old_count = Actor.count
+    delete :destroy, :id => 1
+    assert_equal old_count-1, Actor.count
+    
+    assert_redirected_to actors_path
+  end
+end
