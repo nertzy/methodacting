d0733c01e3ddd5c5ead97e194dffbcf9748135f7
nertzy <nertzy@0ec1f860-a52e-0410-ac07-8d77aaf29911>
2007-04-22 05:41:11 +0000

    Add connections

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,17 +1,23 @@
 # == Schema Information
-# Schema version: 3
+# Schema version: 4
 #
 # Table name: actors
 #
 #  id         :integer(11)   not null, primary key
 #  name       :string(255)   
-#  created_on :date          
-#  updated_on :date          
+#  created_on :datetime      
+#  updated_on :datetime      
 #  active     :boolean(1)    
 #  creator_id :integer(11)   
 #
 
 class Actor < ActiveRecord::Base
   belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
+  has_many :inbound_connections, :class_name => 'Connection', :foreign_key => 'target_id'
+  has_many :outbound_connections, :class_name => 'Connection', :foreign_key => 'source_id'
   validates_presence_of :name
+
+  def connections
+    self.inbound_connections + self.outbound_connections
+  end
 end
---
+++ app/models/connection.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+# Schema version: 4
+#
+# Table name: connections
+#
+#  id          :integer(11)   not null, primary key
+#  source_id   :integer(11)   
+#  target_id   :integer(11)   
+#  creator_id  :integer(11)   
+#  tag         :string(255)   
+#  description :string(255)   
+#
+
+class Connection < ActiveRecord::Base
+  belongs_to :source, :class_name => 'Actor', :foreign_key => 'source_id'
+  belongs_to :target, :class_name => 'Actor', :foreign_key => 'target_id'
+  belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
+  validates_presence_of :tag
+  validates_uniqueness_of :target_id, :scope => [:source_id, :tag]
+end
