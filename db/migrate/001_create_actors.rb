f4c94d7c36b803044dc30e394ad35992fb027de1
nertzy <nertzy@0ec1f860-a52e-0410-ac07-8d77aaf29911>
2007-04-22 01:08:02 +0000

    Use datetime instead of date

--- db/migrate/001_create_actors.rb
+++ db/migrate/001_create_actors.rb
@@ -2,8 +2,8 @@ class CreateActors < ActiveRecord::Migration
   def self.up
     create_table :actors do |t|
       t.column :name, :string
-      t.column :created_on, :date
-      t.column :updated_on, :date
+      t.column :created_on, :datetime
+      t.column :updated_on, :datetime
       t.column :active, :boolean
     end
   end
--- db/migrate/002_create_users.rb
+++ db/migrate/002_create_users.rb
@@ -4,6 +4,8 @@ class CreateUsers < ActiveRecord::Migration
       t.column :name, :string
       t.column :hashed_password, :string
       t.column :salt, :string
+      t.column :created_on, :datetime
+      t.column :updated_on, :datetime
     end
   end
 
