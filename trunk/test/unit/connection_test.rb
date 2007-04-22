faec4059ba7757d469648ea198ae6ff0340820a1
nertzy <nertzy@0ec1f860-a52e-0410-ac07-8d77aaf29911>
2007-04-22 11:52:35 +0000

    Add tests for connection restrictions

--- trunk/test/unit/connection_test.rb
+++ trunk/test/unit/connection_test.rb
@@ -4,20 +4,38 @@ class ConnectionTest < Test::Unit::TestCase
   fixtures :connections
 
   def test_presence_of_tag
-    @connection = Connection.new(
+    connection = Connection.new(
       :source_id => 1,
       :tag => '',
       :target_id => 2
     )
-    assert !@connection.valid?
+    assert !connection.valid?
   end
 
   def test_uniqueness
-    @connection = Connection.new
-    @connection.source_id = connections(:grant_friend_jason).source_id
-    @connection.target_id = connections(:grant_friend_jason).target_id
-    @connection.tag = connections(:grant_friend_jason).tag
-    assert !@connection.valid?
+    connection = Connection.new
+    connection.source_id = connections(:grant_friend_jason).source_id
+    connection.target_id = connections(:grant_friend_jason).target_id
+    connection.tag = connections(:grant_friend_jason).tag
+    assert !connection.valid?
+  end
+
+  def test_tag
+    connection = Connection.new(
+      :source_id => 1,
+      :target_id => 2,
+      :tag => 'Peer'
+    )
+    assert !connection.valid?
+  end
+  
+  def test_source_is_not_target
+    connection = Connection.new(
+      :source_id => 1,
+      :target_id => 1,
+      :tag => 'Friend'
+    )
+    assert !connection.valid?
   end
 
 end
