6efdad8570af822ee59fc784fe61d72dcb620c4b
nertzy <nertzy@0ec1f860-a52e-0410-ac07-8d77aaf29911>
2007-04-22 11:52:35 +0000

    Add tests for connection restrictions

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -32,7 +32,7 @@ class Connection < ActiveRecord::Base
   validates_length_of :tag, :minimum => 1
   validates_uniqueness_of :target_id, :scope => [:source_id, :tag]
   validates_inclusion_of :tag, :in => CONNECTIONS, :message => "can be #{CONNECTIONS.keys.to_sentence} only"
-  validate :validate_tag
+  validate :validate_tag, :validate_source_is_not_target
   
   private
   
@@ -42,6 +42,10 @@ class Connection < ActiveRecord::Base
         errors.add(:tag, "\"#{tag}\" cannot be between #{source.class} and #{target.class}")
       end
     end
+  end
+  
+  def validate_source_is_not_target
+    errors.add(:target_id, 'cannot be the same as source') if source == target
   end
   
 end
