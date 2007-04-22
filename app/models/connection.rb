96dd67e5df957e96b0bda5481af0387cf639d85e
nertzy <nertzy@0ec1f860-a52e-0410-ac07-8d77aaf29911>
2007-04-22 13:00:21 +0000

    Groups can be friends of groups now

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -17,7 +17,8 @@ class Connection < ActiveRecord::Base
       'Individual' => %w{Group}
     },
     'Friend' => {
-      'Individual' => %w{Individual}
+      'Individual' => %w{Individual},
+      'Group' => %w{Group}
     },
     'Leader' => {
       'Individual' => %w{Group}
@@ -29,7 +30,6 @@ class Connection < ActiveRecord::Base
   belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
   
   validates_presence_of :tag
-  validates_length_of :tag, :minimum => 1
   validates_uniqueness_of :target_id, :scope => [:source_id, :tag]
   validates_inclusion_of :tag, :in => CONNECTIONS.keys, :message => "can be #{CONNECTIONS.keys.to_sentence} only"
   validate :validate_tag, :validate_source_is_not_target
